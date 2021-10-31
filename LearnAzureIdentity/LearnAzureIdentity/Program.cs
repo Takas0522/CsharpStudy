1bacdf1006ef3484d63d20606ad672160138e8da
Takas0522 <t_o_19880522@outlook.com>
2021-10-31 18:05:34 +0900

    途中まで

--- LearnAzureIdentity/LearnAzureIdentity/Program.cs
+++ LearnAzureIdentity/LearnAzureIdentity/Program.cs
@@ -2,8 +2,10 @@
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
 using Microsoft.Identity.Web;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace LearnAzureIdentity
@@ -36,14 +38,17 @@ namespace LearnAzureIdentity
         private static string ClientId = "";
         private static string TenantId = "";
         private static string CertPath = "";
+        private static string AuthCode = "";
+        private static string ClientSecret = "";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
+
             SettingsInit();
 
-            var cred = GenTokenCredential(CertType.ClientCertificateCredential);
+            var cred = GenTokenCredential(CertType.InteractiveBrowserCredential);
             var client = new SecretClient(new Uri(KeyVaultUrl), cred);
             var d = client.GetSecret("test-hoge");
             var val = d.Value;
@@ -59,6 +64,8 @@ namespace LearnAzureIdentity
             ClientId = configuration["ClientId"];
             TenantId = configuration["TenantId"];
             CertPath = configuration["CertPath"];
+            AuthCode = configuration["AuthCode"];
+            ClientSecret = configuration["ClientSecret"];
         }
 
         private static TokenCredential GenTokenCredential(CertType certType)
@@ -70,37 +77,55 @@ namespace LearnAzureIdentity
                     // Azure KVで証明書生成してPFXファイル出力してCertPath通したらいけた
                     return new ClientCertificateCredential(TenantId, ClientId, CertPath);
 
-
-
-
                 case CertType.AuthorizationCodeCredential:
-                    // ユーザー情報込のやつ。/authで取得したAuthorizationCodeを使用する?
-                    return new AuthorizationCodeCredential("","","","");
+                    // ユーザー情報込のやつ。/authで取得したAuthorizationCodeを使用する
+                    return new AuthorizationCodeCredential(TenantId, ClientId, ClientSecret, AuthCode, new AuthorizationCodeCredentialOptions { RedirectUri = new Uri(@"http://localhost/") });
+
                 case CertType.AzureCliCredential:
-                    // Azure CLIを使用している場合これを介して取得できる?
-                    return new AzureCliCredential();
+                    // Azure CLIを使用している場合これを介して取得できる
+                    return new AzureCliCredential(new AzureCliCredentialOptions { TenantId = TenantId });
+
                 case CertType.AzurePowerShellCredential:
-                    // PowerShellコマンドでAzureへの認証を通している場合これを介して取得できる?
-                    return new AzurePowerShellCredential();
+                    // PowerShellコマンドでAzureへの認証を通している場合これを介して取得できる
+                    return new AzurePowerShellCredential(new AzurePowerShellCredentialOptions { TenantId = TenantId });
+
                 case CertType.ChainedTokenCredential:
-                    // 複数のTokenCredentialを組み合わせる場合に使用。多分DefaultCredと同じようなことができる？
-                    return new ChainedTokenCredential();
+                    // 複数のTokenCredentialを組み合わせる場合に使用。DefaultCredと同じようなことができる
+                    // PowerShell -> AzureCLI -> ClientCertificate
+                    var first = new AzurePowerShellCredential(new AzurePowerShellCredentialOptions { TenantId = TenantId });
+                    var secound = new AzureCliCredential(new AzureCliCredentialOptions { TenantId = TenantId });
+                    var last = new ClientCertificateCredential(TenantId, ClientId, CertPath);
+                    var list = new List<TokenCredential> { first, secound, last };
+                   
+                    return new ChainedTokenCredential(list.ToArray());
+
                 case CertType.ClientSecretCredential:
                     // シークレット認証?
-                    return new ClientSecretCredential("", "", "");
+                    return new ClientSecretCredential(TenantId, ClientId, ClientSecret);
+
                 case CertType.DefaultAzureCredential:
-                    // 通常の。Env->CLIのあれの順番
+                    // 通常の。
                     return new DefaultAzureCredential();
+
                 case CertType.DeviceCodeCredential:
+                    // デバイスコード認証
                     return new DeviceCodeCredential();
+
                 case CertType.EnvironmentCredential:
+                    // 環境変数
                     return new EnvironmentCredential();
+
                 case CertType.InteractiveBrowserCredential:
-                    // ユーザーにID/Pass入力してもらうあれ？
-                    return new InteractiveBrowserCredential();
+                    // ユーザーにID/Pass入力してもらうあれ
+                    return new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions { ClientId = ClientId, TenantId = TenantId, RedirectUri= new Uri(@"http://localhost/") });
+
                 case CertType.ManagedIdentityCredential:
-                    // ManagedIdentity？
+                    // ManagedIdentity
                     return new ManagedIdentityCredential();
+
+
+                // 以下次回
+
                 case CertType.OnBehalfOfCredential:
                     // OnBeHalfOfFlow。アクセストークンとシークレット
                     return new OnBehalfOfCredential("","","","");
